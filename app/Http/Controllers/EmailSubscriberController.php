efc7eeb8230fa3b44a397d3d380a75cf9b3c73d5
Anton Kefilev <kefilev@yahoo.com>
2025-03-06 15:27:37 +0200

    Let users subscribe and unsubscribe

--- app/Http/Controllers/EmailSubscriberController.php
+++ app/Http/Controllers/EmailSubscriberController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EmailSubscriber;
 use Illuminate\Http\Request;
 
 class EmailSubscriberController extends Controller
 {
-    //
+    public function subscribe(Request $request) {
+        try {
+            //Validate params
+            $validated = $request->validate([
+                'email' => 'bail|required||email:rfc,dns|unique:email_subscribers',
+                'location' => 'required|string',
+            ]);
+
+            //Save to DB
+            $subscriber = new EmailSubscriber();
+            $subscriber->email = $request->query('email');
+            $subscriber->location = $request->query('location');
+            $subscriber->save();
+
+            //Schedule a notification email to let the subscriber know that he has subscibed
+            //TODO
+        } catch (\Exception $e) {
+            // return eror
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+
+        //Return OK
+        return response()->json(['success' => "You have successfully subscribed your email - {$validated['email']} for the daily weather report"], 200);
+    }
+
+    
+    public function unsubscribe(Request $request) {
+        try {
+            //Validate params
+            $validated = $request->validate([
+                'email' => 'bail|required||email:rfc,dns|exists:email_subscribers'
+            ]);
+
+            //Soft Delete from DB
+            EmailSubscriber::where('email', $validated['email'])->delete();
+
+            //Schedule a notification to let the subscriber know that he is no longer a subscriber
+            //TODO
+        } catch (\Exception $e) {
+            // return eror
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+
+        //Return OK
+        return response()->json(['success' => "You have successfully unsubscribed your email - {$validated['email']} from the daily weather report"], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\EmailSubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/subscribe/{email}/{location}', function (Request $request, string $email, string $location) {
-    return $email . ' ' . $location;
-});
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::controller(EmailSubscriberController::class)->group(function () {
+    Route::get('/subscribe', 'subscribe'); //use get to let users subscribe even from the browser
+    Route::get('/unsubscribe', 'unsubscribe'); //using get to let users unsubscribe by visiting a link
+}); //->middleware('auth:sanctum')
