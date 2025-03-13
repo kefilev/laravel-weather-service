d44158c3bb8192b40a320432c5dac5c0bc55a33e
Anton Kefilev <kefilev@yahoo.com>
2025-03-13 15:21:47 +0200

    Add throttle middleware for subscriber routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\EmailSubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(EmailSubscriberController::class)->group(function () {
+Route::middleware(['throttle:api_subscriber'])->controller(EmailSubscriberController::class)->group(function () {
     Route::get('/subscribe', 'subscribe')->name('subscribe'); //use get to let users subscribe even from the browser
     Route::get('/unsubscribe', 'unsubscribe')->name('unsubscribe'); //using get to let users unsubscribe by visiting a link
 }); //->middleware('auth:sanctum')
